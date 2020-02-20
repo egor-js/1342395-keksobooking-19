20e65cc1b09fe562903daea86852fe0735b83dca
Egor <egor@serverice.ru>
2020-02-20 15:27:14 +0300

    commit1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 'use strict';
 
-var mapPins = document.querySelector('.map__pins');
+
 var pinTemplate = document.querySelector('#pin').content.querySelector('button');
 var pinCardTemplate = document.querySelector('#card').content;
-var pins = new Array(8);
-var PLACE_TYPE = ['palace', 'flat', 'house', 'bungalo'];
-var CHECKIN_TIME = ['12:00', '13:00', '14:00'];
-var CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
-var PLACE_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var pins = [];
+var mapPins = document.querySelector('.map__pins');
 var CORRECTION_PIN_X = 25; // смещение по X от точки вставки тега "button" до середины картинки
 var CORRECTION_PIN_Y = 70; // смещение по Y от точки вставки тега "button" до хвостика внизу картинки
 var CORRECTION_RED_PIN_X = 32.5;
@@ -38,7 +35,6 @@ form.capacity.selectedIndex = 2;
 //   console.log(e);
 // };
 
-
 var fieldsetList = document.querySelector('.ad-form').querySelectorAll('fieldset');
 for (var i = 0; i < fieldsetList.length; i++) {
   fieldsetList[i].setAttribute('disabled', '');
@@ -65,6 +61,13 @@ function activateMap(evt) {
 
     buttonStart.removeEventListener('mousedown', activateMap);
     buttonStart.removeEventListener('keydown', activateMap);
+
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < pins.length; i++) {
+      fragment.appendChild(renderPin(i));
+    }
+    mapPins.appendChild(fragment);
+
   }
 }
 
@@ -159,71 +162,20 @@ form.capacity.addEventListener('input', function (evt) {
 });
 
 // var pinCardTemplate = document.querySelector('#card').content;
-// функция генерации случайного положительного целого в заданных пределах, или от 0 до одного значения
-var genRandomInt = function (firstParam, secondParam) {
-  if (!secondParam) {
-    return Math.round(Math.random() * firstParam);
-  } else {
-    return Math.round(Math.random() * (secondParam - firstParam) + firstParam);
-  }
-};
-// генерация массива случайной длинны из ссылок на фотографии объекта аренды
-var genPlacePhotos = function () {
-  var photos = [];
-  for (var i = 0; i < genRandomInt(1, 3); i++) {
-    photos[i] = 'http://o0.github.io/assets/images/tokyo/hotel' + (i + 1) + '.jpg';
-  }
-  return photos;
-};
-var genPlaceFeatures = function () {
-  var features = [];
-  for (var i = 0; i < PLACE_FEATURES.length; i++) {
-    if (Math.random() > 0.5) {
-      features.push(PLACE_FEATURES[i]);
-    }
-  }
-  return features;
-};
+
 // функция создания и заполнения свойств объекта аренды
-var createPins = function () {
-  for (var i = 0; i < pins.length; i++) {
-    var avatarImg = 'img/avatars/user0' + (i + 1) + '.png';
-    pins[i] = {
-      author: {
-        avatar: avatarImg
-      },
-      offer: {
-        title: 'Название объявления №' + i,
-        address: '600, 350',
-        price: genRandomInt(1000, 50000),
-        type: PLACE_TYPE[genRandomInt(PLACE_TYPE.length)],
-        rooms: genRandomInt(1, 4),
-        guests: genRandomInt(1, 20),
-        checkin: CHECKIN_TIME[genRandomInt(CHECKIN_TIME.length - 1)],
-        checkout: CHECKOUT_TIME[genRandomInt(CHECKOUT_TIME.length - 1)],
-        features: genPlaceFeatures(),
-        description: 'Описание предложения №' + i,
-        photos: genPlacePhotos()
-      },
-      location: {
-        x: genRandomInt(0, mapPins.offsetWidth), // диапазон координат по X
-        y: genRandomInt(200, 655) // диапазон координат по Y "до горизонта" с учётом смещения
-      }
-    };
-  }
-};
+pins = window.data.createPins();
+// console.log(pins);
 
-createPins();
-
-var renderPin = function (index) {
+function renderPin(index) {
   var pinElement = pinTemplate.cloneNode(true);
   pinElement.style = ('left: ' + (pins[index].location.x - CORRECTION_PIN_X) + 'px; top: ' + (pins[index].location.y - CORRECTION_PIN_Y) + 'px;');
   pinElement.querySelector('img').src = pins[index].author.avatar;
   pinElement.querySelector('img').alt = pins[index].offer.title;
   return pinElement;
-};
+}
 
-var offerTypeTranslate = function (type) {
+function offerTypeTranslate(type) {
   switch (type) {
     case 'flat':
       type = 'Квартира';
@@ -241,9 +193,8 @@ var offerTypeTranslate = function (type) {
       type = 'AnyAppartments';
   }
   return type;
-};
-
-var insertPhotos = function (template, index) {
+}
+function insertPhotos(template, index) {
   var photosFragment = document.createDocumentFragment();
   for (i = 0; i < pins[index].offer.photos.length; i++) {
     var tempPhotoFragment = template.cloneNode(true);
@@ -251,9 +202,9 @@ var insertPhotos = function (template, index) {
     photosFragment.appendChild(tempPhotoFragment);
   }
   return photosFragment;
-};
+}
 
-var insertFeatures = function (index) {
+function insertFeatures(index) {
   var featuresFragment = document.createDocumentFragment();
   var featuresFragmentUl = document.createElement('ul');
   featuresFragmentUl.classList.add('popup__features');
@@ -266,11 +217,11 @@ var insertFeatures = function (index) {
   }
   featuresFragment.appendChild(featuresFragmentUl);
   return featuresFragment;
-};
+}
 
 var pinCards = document.createDocumentFragment();
 
-var fillCardAd = function (index) {
+function fillCardAd(index) {
   var pinCardTemp = document.createDocumentFragment();
   pinCardTemp = pinCardTemplate.cloneNode(true);
   var imgFromTemplate = pinCardTemp.querySelector('.popup__photos').querySelector('img');
@@ -284,18 +235,12 @@ var fillCardAd = function (index) {
   pinCardTemp.querySelector('.popup__description').textContent = pins[index].offer.description;
   pinCardTemp.querySelector('.popup__photos').replaceChild(insertPhotos(imgFromTemplate, index), imgFromTemplate);
   pinCardTemp.querySelector('.popup__avatar').src = pins[index].author.avatar;
-};
-
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < pins.length; i++) {
-  // fragment.appendChild(renderPin(i));
 }
-mapPins.appendChild(fragment);
 
-fillCardAd(genRandomInt(6)); // заполняем карточку случайного объявления
+fillCardAd(window.data.randomInt(6)); // заполняем карточку случайного объявления
 
 var map = document.querySelector('.map');
 var beforeElement = map.querySelector('div.map__filters-container');
 map.insertBefore(pinCards, beforeElement);
 // console.log(pinCards);
-// beforeElement.before(pinCards);
+beforeElement.before(pinCards);
