339663ef36eab3e9042d38b2a761d2d2d4668ab9
Egor <egor@serverice.ru>
2020-03-21 05:17:51 +0300

    clean main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// var pinsMoki = [];
 var mapPins = document.querySelector('.map__pins');
 var CORRECTION_RED_PIN_X = 32.5;
 var CORRECTION_RED_PIN_Y = 80;
@@ -14,19 +13,11 @@ var buttonStart = document.querySelector('.map__pin.map__pin--main');
 
 buttonStart.setAttribute('tabindex', '0');
 form.address.setAttribute('readonly', '');
-// console.log(parseInt(buttonStart.style.left));
-// console.log(parseInt(buttonStart.style.top));
 var addressX = buttonStart.getBoundingClientRect().x - mapPins.getBoundingClientRect().x + CORRECTION_START_PIN_X;
 var addressY = buttonStart.getBoundingClientRect().y - mapPins.getBoundingClientRect().y + CORRECTION_START_PIN_Y;
 form.address.value = addressX + ', ' + addressY;
 form.address.setAttribute('placeholder', addressX + ', ' + addressY);
 
-// функция создания и заполнения свойств объекта аренды
-// pinsMoki = window.data.createPins();
-
-// var pinCards = document.createDocumentFragment();
-
-// var map = document.querySelector('.map');
 var fieldsetList = document.querySelector('.ad-form').querySelectorAll('fieldset');
 for (var i = 0; i < fieldsetList.length; i++) {
   fieldsetList[i].setAttribute('disabled', '');
@@ -35,8 +26,7 @@ for (i = 0; i < filterForm.length; i++) {
   filterForm[i].setAttribute('disabled', '');
 }
 
-
-function activateMap(evt) {
+function onActivateMap(evt) {
   if (evt.button === 0 || evt.key === 'Enter') {
     document.querySelector('.map').classList.remove('map--faded');
     form.classList.remove('ad-form--disabled');
@@ -48,56 +38,35 @@ function activateMap(evt) {
     for (i = 0; i < filterForm.length; i++) {
       filterForm[i].removeAttribute('disabled', '');
     }
-    buttonStart.removeEventListener('mousedown', activateMap);
-    buttonStart.removeEventListener('keydown', activateMap);
+    buttonStart.removeEventListener('mousedown', onActivateMap);
+    buttonStart.removeEventListener('keydown', onActivateMap);
 
     window.renderPins(window.dataPinsOriginal);
-    // var mapFiltersForm = document.querySelector('.map__filters');
-    // console.log(mapFiltersForm);
-    // window.renderPins(window.filter.byPrice(window.filter.byType('house'), 50000));
-    // window.renderPins(window.filter.byPrice(window.filter.byType(window.dataPinsOriginal, 'flat'), 'middle'));
-    // window.renderPins(window.filter.byType(window.dataPinsOriginal, 'flat'));
     var mapFiltersForm = document.querySelector('form.map__filters');
-    var selectType = mapFiltersForm.querySelector('#housing-type');
-    mapFiltersForm.addEventListener('input', function (evtFilter) {
-
-      // window.renderPins(window.filter.all(window.dataPinsOriginal));
+    mapFiltersForm.addEventListener('input', function () {
+      window.card.close();
       window.renderPins(window.filter.all(window.dataPinsOriginal));
-      var target = evtFilter.target;
-      // console.log(target);
-      // console.log(selectType.value);
-      // if (target === selectType) {
-      //   window.filter.byType(window.dataPinsOriginal, target.value);
-      //   // window.removePins();
-      // }
-      // window.renderPins(window.filter.byType(window.dataPinsOriginal, target.value));
     });
-
   }
-
-
 }
-
-function doAction(evt) {
+function onDoAction(evt) {
   if (evt.key === 'Escape') {
     window.card.close();
   }
 }
-
-buttonStart.addEventListener('mousedown', activateMap);
-buttonStart.addEventListener('keydown', activateMap);
-buttonStart.addEventListener('mousedown', movePin);
+buttonStart.addEventListener('mousedown', onActivateMap);
+buttonStart.addEventListener('keydown', onActivateMap);
+buttonStart.addEventListener('mousedown', onMovePin);
 document.addEventListener('click', window.card.renderCard);
-document.addEventListener('keydown', doAction);
+document.addEventListener('keydown', onDoAction);
 
-function movePin(evt) {
+function onMovePin(evt) {
   evt.preventDefault();
   if (evt.button === 0) {
     var startCoords = {
       x: evt.clientX,
       y: evt.clientY
     };
-
   }
   function onMouseMove(moveEvt) {
     moveEvt.preventDefault();
@@ -127,7 +96,6 @@ function movePin(evt) {
     }
     buttonStart.style.top = finishCoords.y + 'px';
     buttonStart.style.left = finishCoords.x + 'px';
-
   }
   function onMouseUp(upEvt) {
     upEvt.preventDefault();
@@ -140,15 +108,3 @@ function movePin(evt) {
   document.addEventListener('mousemove', onMouseMove);
   document.addEventListener('mouseup', onMouseUp);
 }
-
-// var errorHandler = function (errorMessage) {
-//   var node = document.createElement('div');
-//   node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-//   node.style.position = 'absolute';
-//   node.style.left = 0;
-//   node.style.right = 0;
-//   node.style.fontSize = '30px';
-//
-//   node.textContent = errorMessage;
-//   document.body.insertAdjacentElement('afterbegin', node);
-// };
